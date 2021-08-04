519670b275d7172237b96993f62db8eb4ac1b675
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-08-03 20:30:08 -0700

    gemini.Request

--- handler.go
+++ handler.go
@@ -2,5 +2,5 @@ package gemini
 
 // A Handler responds to a Gemini Protocol (or Naked) request.
 type Handler interface {
-	ServeGemini(ResponseWriter, string)
+	ServeGemini(ResponseWriter, Request)
 }
