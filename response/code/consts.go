874e8c3df36d52cccf952b89ee5545fdd4ce0973
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-08-04 23:17:27 -0700

    geminiresponsecode

---
+++ response/code/consts.go
@@ -0,0 +1,20 @@
+package geminiresponsecode
+
+const Input                     = 10
+const SensitiveInput            = 11
+const Success                   = 20
+const RedirectTemporary         = 30
+const RedirectPermanent         = 31
+const TemporaryFailure          = 40
+const ServerUnavailable         = 41
+const CGIError                  = 42
+const ProxyError                = 43
+const SlowDown                  = 44
+const PermanentFailure          = 50
+const NotFound                  = 51
+const Gone                      = 52
+const ProxyRequestRefused       = 53
+const BadRequest                = 59
+const ClientCertificateRequired = 60
+const CertificateNotAuthorised  = 61
+const CertificateNotValid       = 62
