c9429d8f3c592208b63a1dce53c0d40e44582375
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-08-05 12:26:16 -0700

    geminirequest

--- request/parse.go
+++ request/parse.go
@@ -7,15 +7,25 @@ import (
 	"strings"
 )
 
+// Max represents the maximum number of bytes that a permitted, by the Gemini Protocol specification,
+// for a Gemini Protocol request-line.
 const Max = 1024
 
 // Parse parses a Gemini Protocol request-line, and returns the target.
+//
+// For example, if the Gemini Protocol request-line was:
+//
+//	"gemini://example.com/path/to/something\r\n"
+//
+// The Parse would return:
+//
+//	"gemini://example.com/path/to/something"
 //
 // Gemini Protocol requests are a single CRLF-terminated line with the following structure:
 //
 // <TARGET><CR><LF>
 //
-// Where “<TARGET>” is a UTF-8 encoded string, of maximum length 1024 bytes.
+// Where “<TARGET>” is a UTF-8 encoded string, with maximum length of 1024 bytes.
 //
 // Typically “<TARGET>” is an absolute URL, including the scheme.
 //
@@ -32,6 +42,8 @@ const Max = 1024
 // hash://sha256/0ba904eae8773b70c75333db4de2f3ac45a8ad4ddba1b242f0b3cfc199391dd8
 //
 // data:text/plain;charset=utf-8;base64,VGhpcyBpcyBhIHRlc3Qh
+//
+// Note that the when the target is a URL, it does NOT have to be a gemini-URL.
 //
 // It is up to a Gemini Protocol server to decide which target URLs it wants to respond to with a `20 SUCCESS` or not.
 // This function just tries to parse it, and returns it.
